929e55ac96dc488699f29acec560ba13049a7fd9
Brian Moriguchi <brian@beckos.com>
2022-01-04 17:47:34 -0800

    Initial Commit

---
+++ RESTAPIRequest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RESTAPIRequest
+//
+//  Created by Tatsuya Moriguchi on 1/4/22.
+//  Copyright © 2022 Tatsuya Moriguchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
